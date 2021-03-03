5392ce4c792df39c222d5fd7f1a33777e67064eb
leee1846 <leee1846@gmail.com>
2021-03-03 22:56:44 +0900

    feature: login컴포넌트 구석
    -useForm 커스텀 훅 적용

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -24,10 +24,8 @@ const FETCH_POSTS_QUERY = gql`
 `;
 
 const Home = () => {
-  const {
-    loading,
-    data: { getPosts: posts },
-  } = useQuery(FETCH_POSTS_QUERY);
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+  const { getPosts: posts } = data;
 
   return (
     <Grid columns={3}>
