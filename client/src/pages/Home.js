e2dee1f89c7c82e874095ae112bf779f792066a2
leee1846 <leee1846@gmail.com>
2021-03-03 20:28:50 +0900

    style: home, postCard 컴포넌트 스타일 구성

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,52 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import { Grid, Image } from "semantic-ui-react";
+import PostCard from "../components/PostCard";
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      likeCount
+      commentCount
+      likes {
+        username
+      }
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 
 const Home = () => {
+  const {
+    loading,
+    data: { getPosts: posts },
+  } = useQuery(FETCH_POSTS_QUERY);
+
   return (
-    <div>
-      <h1>home</h1>
-    </div>
+    <Grid columns={3}>
+      <Grid.Row>
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading posts..</h1>
+        ) : (
+          posts &&
+          posts.map((post) => (
+            <Grid.Column key={post.id}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
   );
 };
 
