0290cb1cb2933cdf045be49b241c6cf0f134394a
leee1846 <leee1846@gmail.com>
2021-03-03 18:03:05 +0900

    feature: 브라우저 라우터 연결

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import { client } from "./ApolloProvider";
 import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "sementic-ui-css/sementic.min.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <h1>asdasd</h1>
+      <Router>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/login'>
+          <Login />
+        </Route>
+        <Route exact path='/register'>
+          <Register />
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
