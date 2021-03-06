23b12b451de1a873d32c8a2afacd5d2515496ba2
leee1846 <leee1846@gmail.com>
2021-03-06 13:23:28 +0900

    test: singlePost 코멘트 리스트

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { gql, useQuery } from "@apollo/client";
-import { Card } from "semantic-ui-react";
+import { Card, CardContent } from "semantic-ui-react";
 import { AuthContext } from "../context/auth";
 import { Grid, Image, Button, Icon, Label } from "semantic-ui-react";
 import LikeButton from "../components/LikeButton";
@@ -32,8 +32,6 @@ const SinglePost = (props) => {
   const postId = props.match.params.postId;
   const { user } = useContext(AuthContext);
 
-  console.log(postId);
-
   const { data } = useQuery(FETCH_POST_QUERY, {
     variables: {
       postId,
@@ -54,7 +52,7 @@ const SinglePost = (props) => {
     postMarkup = (
       <Grid>
         <Grid.Row>
-          <Grid.Column width={2}>
+          <Grid.Column width={3}>
             <Image
               src='https://react.semantic-ui.com/images/avatar/large/molly.png'
               size='small'
@@ -62,30 +60,41 @@ const SinglePost = (props) => {
             />
           </Grid.Column>
           <Grid.Column width={10}>
-            <Card.Content>
-              <Card.Header>이름</Card.Header>
-              <Card.Meta>몇시간전...</Card.Meta>
-              <Card.Description>{body}</Card.Description>
-            </Card.Content>
-            <hr />
-            <Card.Content extra>
-              <LikeButton user={user} post={{ id: likeCount, likes }} />
-              <Button
-                as='div'
-                labelPosition='right'
-                onClick={() => console.log("comment on post")}
-              >
-                <Button basic color='blue'>
-                  <Icon name='comments' />
+            <Card fluid>
+              <Card.Content>
+                <Card.Header>이름</Card.Header>
+                <Card.Meta>몇시간전...</Card.Meta>
+                <Card.Description>{body}</Card.Description>
+              </Card.Content>
+              <hr />
+              <Card.Content extra>
+                <LikeButton user={user} post={{ id: likeCount, likes }} />
+                <Button
+                  as='div'
+                  labelPosition='right'
+                  onClick={() => console.log("comment on post")}
+                >
+                  <Button basic color='blue'>
+                    <Icon name='comments' />
+                  </Button>
+                  <Label basic color='blue' pointing='left'>
+                    {commentCount}
+                  </Label>
                 </Button>
-                <Label basic color='blue' pointing='left'>
-                  {commentCount}
-                </Label>
-              </Button>
-              {user && (
-                <DeleteButton postId={id} callback={deletePostCallback} />
-              )}
-            </Card.Content>
+                {user && (
+                  <DeleteButton postId={id} callback={deletePostCallback} />
+                )}
+              </Card.Content>
+            </Card>
+            {comments.map((comment) => (
+              <Card fluid key={comment.id}>
+                <Card.Content>
+                  <Card.Header>{comment.username}</Card.Header>
+                  <Card.Meta>시간</Card.Meta>
+                  <Card.Description>{comment.body}</Card.Description>
+                </Card.Content>
+              </Card>
+            ))}
           </Grid.Column>
         </Grid.Row>
       </Grid>
